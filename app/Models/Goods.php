67c8c07ed949dc3e17ed09a4291dc8ff3aa7b450
Oopsfix <xvab@yandex.ru>
2022-07-30 21:40:49 +0400

    attach option

--- app/Models/Goods.php
+++ app/Models/Goods.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use App\Shop\Goods\Enums\GoodsType;
+use Illuminate\Support\Facades\DB;
 
 class Goods extends Model
 {
@@ -28,5 +29,16 @@ class Goods extends Model
     public function options()
     {
         return $this->belongsToMany(GoodsOption::class, 'goods_ref_options', 'goods_id', 'option_id')->withTimestamps();
+    }
+    
+    public function attachOption(int $optionId,  int $userId)
+    {
+        $option = GoodsOption::findOrFail($optionId);
+        
+        $this->options()->attach($optionId, [
+            'sortpos' => DB::select('SELECT COUNT(*) as count FROM goods_ref_options WHERE goods_id=?', [$this->id])->value('count'),
+            'own_user_id' => $option->user_id,
+            'set_user_id' => $userId
+        ]); 
     }
 }

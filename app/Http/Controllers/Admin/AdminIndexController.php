e9c4cbbf2c4c5e4e7307a127b5fdabb329931f12
Oopsfix <xvab@yandex.ru>
2022-08-01 13:55:06 +0400

    old trash

--- app/Http/Controllers/Admin/AdminIndexController.php
+++ app/Http/Controllers/Admin/AdminIndexController.php
@@ -11,8 +11,7 @@ class AdminIndexController extends AdminBaseController
     //
     public function index(Request $request)
     {
-        //Inertia::setRootView('admin.admin_app.blade.php');
-
+        
         return Inertia::render('HomeView');
     }
 }

4928cb7f2e09167f487bc283e604b7312f801984
2byte <oopsfix21@gmail.com>
2024-01-05 11:55:05 +0300

    next

--- app/Models/Traits/UploadFileTrait.php
+++ app/Models/Traits/UploadFileTrait.php
@@ -12,11 +12,11 @@ use Storage;
 trait UploadFileTrait {
 
   /**
-   * 
+   *
    * @return [
    *   [
-   *    'path' => '/path', 
-   *    'path_destination' => '/path', 
+   *    'path' => '/path',
+   *    'path_destination' => '/path',
         'filename' => 'filename.jpg',
         'is_img' => true,
         'size' => 123,
@@ -28,43 +28,43 @@ trait UploadFileTrait {
    * ]
    **/
   public function uploadFile(Request $request) {
-    
+
     $files = $request->file('files');
-    
-    $checkIsValidFile = function ($file) {
+
+    $checkIsValidFile = function ($file) use($request) {
       if (!$file->isValid()) {
         throw ValidationException::withMessages([
           'files' => $request->files->all()['files']->getErrorMessage()
         ]);
       }
     };
-    
+
     if (!is_array($files)) {
       $files = [$files];
     }
-    
+
     foreach ($files as $file) {
       $checkIsValidFile($file);
     }
-    
+
     $validationRules = ['required', 'file'];
-    
+
     if (!empty($this->uploadAccessibleExtensions)) {
       $validationRules[] = 'mimes:'. implode(',', $this->uploadAccessibleExtensions);
     }
     if (!empty($this->uploadMaxFilesizeKb)) {
       $validationRules[] = 'between:20,'. $this->uploadMaxFilesizeKb;
     }
-    
+
     // checking image sizes and ratio
     $ruleImageDimensions = Rule::dimensions()
       ->minWidth($this->uploadImageDimensions[0] ?? 0)
       ->minHeight($this->uploadImageDimensions[1] ?? 0);
-    
+
     if (!is_null($this->uploadImageRatio)) {
       $ruleImageDimensions->ratio($this->uploadImageRatio);
     }
-    
+
     $request->validate([
       'files' => 'required',
       'files.*' => $validationRules,
@@ -72,19 +72,19 @@ trait UploadFileTrait {
     ], [
       'files.required' => 'Файлы не получены'
     ]);
-    
+
     $files = is_array($request->file('files')) ? $request->file('files') : [$request->file('files')];
-    
+
     $aliasModel = $request->model;
     $userId = $request->user()->id;
-    
+
     $uploadedFiles = array_map(function ($file) use ($aliasModel, $userId) {
-      
+
       $isImage = str_contains($file->getClientMimeType(), 'image');
-      
+
       $path = $file->store($this->uploadDir .'/'. $userId);
       $pathDestination = storage_path('app/'. $path);
-      
+
       $returnData = [
         'path' => $path,
         'path_destination' => $path,
@@ -92,28 +92,28 @@ trait UploadFileTrait {
         'is_img' => $isImage,
         'size' => $file->getSize(),
       ];
-      
+
       // Resizing images
       if (
-        $isImage 
-        && isset($this->uploadImageResizeSizes) 
+        $isImage
+        && isset($this->uploadImageResizeSizes)
         && is_array($this->uploadImageResizeSizes)
         && !empty($this->uploadImageResizeSizes)
         ) {
         foreach ($this->uploadImageResizeSizes as $sizes) {
-          
+
           $im = Image::make($pathDestination);
           $im->resize($sizes[0], null, function ($constraint) { $constraint->aspectRatio(); });
           $pathResizedImage = preg_replace('/^(.+)(\..+)$/i', '$1_'. $sizes[0] .'x'. $sizes[1] .'$2', $pathDestination);
           $im->save($pathResizedImage);
-          
+
           $returnData['resized_images'][$sizes[0]] = $pathResizedImage;
         }
       }
-      
-      return $returnData; 
+
+      return $returnData;
     }, $files);
-    
+
     return $uploadedFiles;
   }
 }
--- vite.config.js
+++ vite.config.js
@@ -22,7 +22,7 @@ export default defineConfig({
     ],
     server: {
         watch: {
-            usePolling: true,
+            //usePolling: true,
             ignored: [
                 '/node_modules/**',
                 '/vendor/**'
